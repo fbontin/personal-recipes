4bdbfc158f5fb00e0b4b29460ff70efa08ae2178
Filip Bontin <fbontin@users.noreply.github.com>
2024-07-06 11:14:11 +0200

    Add github actions deployment

--- app/.eleventy.js
+++ app/.eleventy.js
@@ -1,5 +1,10 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy("./src/css");
+  console.log('pathPrefix', eleventyConfig.pathPrefix);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin, { baseHref: '/recipes' });
+
   return ({
     dir: {
       input: 'src',
